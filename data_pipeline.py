37220ffa751fab7faf8c4fa91252678e5e3c065e
Adrian Cosio <adriancosio14@gmail.com>
2025-10-01 17:55:56 -0500

    Add docstring to read_data function.

--- data_pipeline.py
+++ data_pipeline.py
@@ -2,7 +2,16 @@ import pandas as pd
 import os
 
 def read_titanic_data(directory, filename):
+    """ Read csv file and convert to pandas DataFrame
+
+    Ags:
+        directory: path of file
+        filename: name of file
     
+    Returns:
+        titanic_df: DataFrame of csv data
+    
+    """
     try:
         full_file_path = os.join(directory,filename)
         titanic_df = pd.read_csv(full_file_path)
@@ -10,3 +19,4 @@ def read_titanic_data(directory, filename):
         print(f"Error in read_titanic_data function: {e}")
     
     return titanic_df
+
