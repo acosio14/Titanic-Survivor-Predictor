713c597fc1cd0ffffdec0a152570983afb520fc9
Adrian Cosio <adriancosio14@gmail.com>
2025-10-01 18:58:58 -0500

    Add clean_data function.

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import os
 
+from sklearn.preprocessing import LabelEncoder
+
 def read_titanic_data(directory, filename):
-    """ Read csv file and convert to pandas DataFrame
+    """ Read csv file and convert to pandas DataFrame (Data Acquisition)
 
     Ags:
         directory: path of file
@@ -20,3 +22,20 @@ def read_titanic_data(directory, filename):
     
     return titanic_df
 
+def clean_titanic_data(titanic_df):
+    """ Clean DataFrame data."""
+    
+    # Drop irrelevant columns
+    drop_values = ['PassengerId', 'Name', 'Ticket', 'Cabin']
+    train_clean_df = train_clean_df.drop([drop_values], axis=1)   
+
+    # Handle Missing Data
+    train_clean_df = titanic_df[titanic_df['Age'].notna()].dropna()
+
+    # Change categorical columns to numeric
+    # To-Do: Figure out if I need to output original categories or if stored in LableEncoder somehow
+    train_clean_df['Sex'] = LabelEncoder().fit_transform(train_clean_df['Sex'])
+    train_clean_df['Embarked'] = LabelEncoder().fit_transform(train_clean_df['Embarked'])
+
+    return train_clean_df
+    
