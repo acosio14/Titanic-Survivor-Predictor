e9ccdd0c2f444ba55f6e8da13be4ed377a0a28a2
Adrian Cosio <adriancosio14@gmail.com>
2025-10-04 15:43:29 -0500

    Add error catching to save_model, add logic to save model if >80% accuracy.

--- train_pipeline.py
+++ train_pipeline.py
@@ -7,24 +7,28 @@ def logistic_regression(X_train, y_train, X_test, y_test):
     # Initialize Model, fit, and predict
     model = LogisticRegression()
     model.fit(X_train, y_train)
+
     y_pred = model.predict(X_test)
-
     accuracy = accuracy_score(y_test, y_pred)
-
-
-    return round(accuracy * 100,3)
-
-
-
-def save_model(model, filename):
     
-    if not filename.endswith(".pkl"):
-        print(f"Error: {filename} not .pkl")
+    if accuracy >= .8:
+        print(f"Model accuracy: {round(accuracy * 100,3)}%")
+        return model
+    else:
+        print(f"Model accuracy: {round(accuracy * 100,3)}%")
+        print(f"Not returning model.")
+
+
+
+def save_model(model, filepath):
+    
+    if not filepath.endswith(".pkl"):
+        print(f"Error: {filepath} not .pkl")
         return
     try:
-        with open(filename,'wb') as file:
+        with open(filepath,'wb') as file:
             pickle.dump(model, file)
 
-        print(f"Saved {model} in {filename}")
+        print(f"Saved {model} in {filepath}")
     except FileNotFoundError:
         print(f"Error: File not found at {filepath}")
