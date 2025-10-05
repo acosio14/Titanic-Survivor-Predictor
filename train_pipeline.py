2b497379e1bf6635162070d4879ea5ff91064c44
Adrian Cosio <adriancosio14@gmail.com>
2025-10-05 05:49:11 -0500

    Grammar fix.

--- train_pipeline.py
+++ train_pipeline.py
@@ -12,10 +12,10 @@ def logistic_regression(X_train, y_train, X_test, y_test):
     accuracy = accuracy_score(y_test, y_pred)
     
     if accuracy >= .8:
-        print(f"Model accuracy: {round(accuracy * 100,3)}%")
+        print(f"Model Accuracy: {round(accuracy * 100,3)}%")
         return model
     else:
-        print(f"Model accuracy: {round(accuracy * 100,3)}%")
+        print(f"Model Accuracy: {round(accuracy * 100,3)}%")
         print(f"Not returning model.")
 
 def decision_tree():
