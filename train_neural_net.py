45255dab097fe93649f492cae12057641baa501b
Adrian Cosio <adriancosio14@gmail.com>
2025-10-13 19:22:40 -0500

    Complete NeuralNetwork class.

--- train_neural_net.py
+++ train_neural_net.py
@@ -4,12 +4,22 @@ from torch import nn
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer1 = nn.Linear(l1_input_size, l1_output_size  )
-        self.relu = nn.ReLu()
-        self.layer2 = nn.Linear(l1_output_size, l2_output_size)
+        self.layer1 = nn.Linear(7, 100)
+        self.layer2 = nn.Linear(100, 100)
+        self.layer3 = nn.Linear(100, 200)
+        self.layer4 = nn.Linear(200, 100)
+        self.layer5 = nn.Linear(100, 1)
 
+    # x_in is a tensor of shape (7,1)?
     def forward(self,x_in):
         x = self.layer1(x_in)
-        x = self.relu(x)
+        x = nn.ReLU(x)
         x = self.layer2(x)
+        x = nn.ReLU(x)
+        x = self.layer3(x)
+        x = nn.ReLU(x)
+        x = self.layer4(x)
+        x = nn.ReLu(x)
+        x = self.layer5(x)
+
         return x
